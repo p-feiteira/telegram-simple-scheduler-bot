b1686c27be4a19b77b57c457a7af9a93527e7252
Pedro Feiteira <p.feiteira@campus.fct.unl.pt>
2021-10-06 14:48:51 -0700

    feat: add google sheets support

--- simple-sheduler/simple_sheduler/bot/bot.py
+++ simple-sheduler/simple_sheduler/bot/bot.py
@@ -14,13 +14,21 @@ storage = Storage()
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['reserva'])
 def send_welcome(message):
-    storage.add("Feiteira", 2, "16")
-    bot.reply_to(message, "Sup Bich?")
+    name: str = f"{message.from_user.first_name} {message.from_user.last_name}"
+    content: str = message.text.split()
+    logging.info(content)
+    if len(content) <=1 :
+        bot.reply_to(message, "Reserva inválida. Falta número de slots e/ou horas")
+    else:
+        storage.add(name, int(content[1]), content[2])
+        bot.reply_to(message, f"{name} reserva para {content[1]} pessoa(s) às {content[2]} dia 0/0/0000 registada com sucesso.")
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
+@bot.message_handler(commands=['cancelar'])
+def cancel(message):
+    name: str = f"{message.from_user.first_name} {message.from_user.last_name}"
+    storage.remove(name)
+    bot.reply_to(message, message.text)
 
 bot.infinity_polling()
--- simple-sheduler/simple_sheduler/bot/persistent.py
+++ simple-sheduler/simple_sheduler/bot/persistent.py
@@ -1,4 +1,8 @@
 import pygsheets
+import pandas as pd
+import logging
+
+logging.basicConfig(level=logging.INFO)
 class Singleton(type):
     """
     Define an Instance operation that lets clients access its unique
@@ -16,19 +20,35 @@ class Singleton(type):
 class Storage(metaclass=Singleton):
     def __init__(self) -> None:
         #authorization
-        gc = pygsheets.authorize()
+        gc = pygsheets.authorize(client_secret="/home/pedro/Github/telegram-simple-scheduler-bot/client_secret.json")
         #open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
-        sh = gc.open('Cross Persistent')
+        self.__sh = gc.open('Cross Persistent')
         #select the first sheet 
-        self.__df = sh[0].get_as_df()
 
     def add(self, name: str, slots: int, hours:str) -> None:
+        # Get Sheet current state as pandas Dataframe
+        wks = self.__sh[0]
+        #retrieve all rows of the worksheet
+        cells = wks.get_all_values(include_tailing_empty_rows=False, include_tailing_empty=False, returnas='matrix')
+        logging.info(len(cells))
         # Create empty dataframe
-        df = pd.Dataframe()
-        # Create a column
-        df['name'] = [name]
-        df['slots'] = [slots]
-        df['hours'] = [hours]
-        #update the first sheet with df, starting at cell B2. 
-        self.__wks.set_dataframe(df,(1,1))
+        if len(cells) == 1:
+            df = pd.DataFrame([[name, slots, hours]], columns=["name","slots", "hours"])
+            wks.set_dataframe(df,(1,1))
+        else:
+            wks.append_table(values=[name, slots, hours])
+
+        logging.info(wks.get_all_values(include_tailing_empty_rows=False, include_tailing_empty=False, returnas='matrix'))
+        #self.__sh[0].set_dataframe(current_df,(1,1))
+
+    def remove(self, name: str) -> None:
+        wks = self.__sh[0]
+        # Get Sheet current state as pandas Dataframe
+        cells = wks.get_all_values(include_tailing_empty_rows=False, include_tailing_empty=False, returnas='matrix')
+        logging.info(cells)
+        for i in range(1, len(cells)):
+            if(cells[i][0] == name):
+                logging.info(cells[i][0])
+                logging.info(i)
+                wks.delete_rows(i+1)
     
